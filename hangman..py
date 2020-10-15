b231e0812360b566c2c3d2c9b30a8217f1a86fb2
Shubham Aggarwal <shubham2403tayal@gmail.com>
2020-10-15 19:04:01 +0530

    Added Menu Functionality
    Now game is continuous till user wants

--- hangman..py
+++ hangman..py
@@ -17,22 +17,33 @@ while i < 8:
     print(f'\n{word3}', end = ' ')
     if '-' in word3:
         word = input('\nInput a letter: ')
-        if word not in word3:
-            input1.extend(word)
-            word2 = list(word2)
-            word3 = list(word3)
-    #**************************************************************************#
-            if word in word2:
-                for j in range(len(word2)):
-                    if word2[j] == word:
-                        word3[j] = word
-                word3 = ''.join([str(elem) for elem in word3])
-            else:
-                print('No such letter in the word')
-                i +=1
+        
+        if len(word) != 1 or word == ' ':
+            print('You should input a single letter')
+        elif word.isalpha() and word.isupper():
+            print('It is not an ASCII lowercase letter')
+        elif not word.isalpha():
+            print('It is not an ASCII lowercase letter')
         else:
-            print('No improvements')
-            i += 1
+            if word not in input1:
+                if word not in word3:
+                    input1.extend(word)
+                    word2 = list(word2)
+                    word3 = list(word3)
+            #**************************************************************************#
+                    if word in word2:
+                        for j in range(len(word2)):
+                            if word2[j] == word:
+                                word3[j] = word
+                        word3 = ''.join([str(elem) for elem in word3])
+                    else:
+                        print('No such letter in the word')
+                        i +=1
+                else:
+                    print('You already typed this letter')
+                    i += 1
+            else:
+                print('You already typed this letter')
     else:
         break
 
